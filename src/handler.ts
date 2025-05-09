fdea5b728fc872c77d75c5796a91a3654f81524a
Douglas Oliveira <douglas.oliveira@esportudo.com>
2025-05-09 00:39:57 -0300

    ajusta deploy 9

--- src/handler.ts
+++ src/handler.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda with TypeScript!" }),
+    body: JSON.stringify({ message: "Hello from Douglas to Lambda!" }),
   };
 };
