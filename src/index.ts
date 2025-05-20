7f59895459d8e9151ec6ff4eb4fe7e10ced59899
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-20 18:47:37 -0300

    consome API 9

--- src/index.ts
+++ src/index.ts
@@ -7,41 +7,41 @@ const BASE_URL = process.env.BASE_URL ?? "";
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log(event.pathParameters?.cpf);
 
-  try {
-    const cpf = event.pathParameters?.cpf;
+  // try {
+  const cpf = event.pathParameters?.cpf;
 
-    if (!cpf) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "CPF é obrigatório" }),
-      };
-    }
-
-    const token = jwt.sign({ cpf }, SECRET, { expiresIn: "5m" });
-
-    const response = await axios.get(`http://${BASE_URL}/user/${cpf}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
+  if (!cpf) {
     return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello from Douglas to Lambda!",
-        CPF: cpf ?? "Nenhum CPF enviado",
-        baseurl: `http://${BASE_URL}/user`,
-        response: response.data,
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Erro!",
-        error: `Erro => ${error}`,
-        base_url: `http://${BASE_URL}/user`,
-      }),
+      statusCode: 400,
+      body: JSON.stringify({ message: "CPF é obrigatório" }),
     };
   }
+
+  const token = jwt.sign({ cpf }, SECRET, { expiresIn: "5m" });
+
+  const response = await axios.get(`http://${BASE_URL}/user/${cpf}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Douglas to Lambda!",
+      CPF: cpf ?? "Nenhum CPF enviado",
+      baseurl: `http://${BASE_URL}/user/${cpf}`,
+      response: response.data,
+    }),
+  };
+  // } catch (error) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       message: "Erro!",
+  //       error: `Erro => ${error}`,
+  //       base_url: `http://${BASE_URL}/user`,
+  //     }),
+  //   };
+  // }
 };
