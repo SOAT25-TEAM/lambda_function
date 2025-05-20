4d2ae24d91490e12bf6729bb43f7aae26b0e0f75
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-20 18:21:05 -0300

    consome API 2

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,37 @@ import jwt from "jsonwebtoken";
 import axios from "axios";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const cpf = event.pathParameters?.cpf;
-  const SECRET = process.env.JWT_SECRET ?? "";
+  try {
+    const cpf = event.pathParameters?.cpf;
+    const SECRET = process.env.JWT_SECRET ?? "";
 
-  if (!cpf) {
+    if (!cpf) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "CPF é obrigatório" }),
+      };
+    }
+
+    const token = jwt.sign({ cpf }, SECRET, { expiresIn: "5m" });
+
+    const response = await axios.get("http://process.env.BASE_URL/user", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "CPF é obrigatório" }),
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Hello from Douglas to Lambda!",
+        CPF: cpf ?? "Nenhum CPF enviado",
+        response: response.data,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      error: `Error => ${error}`,
     };
   }
-
-  const token = jwt.sign({ cpf }, SECRET, { expiresIn: "5m" });
-
-  const response = await axios.get("http://process.env.BASE_URL/user", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Douglas to Lambda!",
-      CPF: cpf ?? "Nenhum CPF enviado",
-      response: response.data,
-    }),
-  };
 };
