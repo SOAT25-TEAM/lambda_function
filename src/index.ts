ef78d6fa3d3af0c50d3a36eaad0639c5b4c2dd27
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-20 19:04:48 -0300

    consome API 12

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import axios from "axios";
 
 const SECRET = process.env.JWT_SECRET ?? "";
 const BASE_URL = process.env.BASE_URL ?? "";
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   const cpf = event.pathParameters?.cpf;
-  let message;
 
   if (!cpf) {
     return {
@@ -17,23 +17,43 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   const token = jwt.sign({ cpf }, SECRET, { expiresIn: "5m" });
 
-  const response = await axios.get(`http://${BASE_URL}/user/${cpf}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  try {
+    const response = await axios.get(`http://${BASE_URL}/user/${cpf}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  if (response.status === 404) {
-    message = "Usuário não encontrado";
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({
+        message: "Usuário encontrado",
+        CPF: cpf,
+        response: response.data,
+      }),
+    };
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      const status = error.response?.status ?? 500;
+      const errorMessage =
+        status === 404 ? "Usuário não encontrado" : "Erro na requisição";
+
+      return {
+        statusCode: status,
+        body: JSON.stringify({
+          message: errorMessage,
+          CPF: cpf,
+          error: error.response?.data ?? error.message,
+        }),
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Erro interno inesperado",
+        error: error?.message || "Erro desconhecido",
+      }),
+    };
   }
-
-  return {
-    statusCode: response.status,
-    body: JSON.stringify({
-      message,
-      CPF: cpf ?? "Nenhum CPF enviado",
-      baseurl: `http://${BASE_URL}/user/${cpf}`,
-      response: response.data,
-    }),
-  };
 };
