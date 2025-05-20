b17a2d527dee14391084c71287b9f57cc99128df
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-20 17:08:25 -0300

    ajusta para ESM

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import jwt from "jsonwebtoken";
+import axios from "axios";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const cpf = event.pathParameters?.cpf;
+  const SECRET = process.env.JWT_SECRET ?? "";
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello from Douglas to Lambda!",
       CPF: cpf ?? "Nenhum CPF enviado",
+      secret: SECRET,
     }),
   };
 };
